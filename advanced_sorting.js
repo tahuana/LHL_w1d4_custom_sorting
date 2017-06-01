7c5334eb1175c12f57ec655d17598f4809191537
tahuana <tahuana@gmail.com>
2017-06-01 16:26:45 +0000

    advanced sorting

---
+++ advanced_sorting.js
@@ -0,0 +1,27 @@
+var students = [
+  { id: 1, name: "bruce",    age: 40 },
+  { id: 2, name: "zoidberg", age: 22 },
+  { id: 3, name: "alex",     age: 22 },
+  { id: 4, name: "alex",     age: 30 }
+];
+
+
+// sort by name
+students.sort(function(a, b) {
+  var nameA = a.name.toUpperCase(); // ignore upper and lowercase
+  var nameB = b.name.toUpperCase(); // ignore upper and lowercase
+  if (nameA < nameB) {
+    return -1;
+  }
+  if (nameA > nameB) {
+    return 1;
+  }
+  // names must be equal - sort by age
+  {
+    return b.age - a.age;
+  }
+});
+
+console.log(students);
+
+
